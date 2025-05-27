c9d6889b3c51e10f04a942b5cde32f3af62308e8
val <133932492+vilswtry@users.noreply.github.com>
2025-05-26 23:38:12 -0400

    servicio de notificaciones

---
+++ notificaciones/src/test/java/com/GreenEnergy/notificacionesPrueba/NotificacionesPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GreenEnergy.notificacionesPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionesPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
